d9a25f363856dfe5bf3793674bd02d8921b3efe5
bwoodward523 <bwoodward@ycp.edu>
2025-05-05 11:43:01 -0400

    Prim's alg in progress

--- 3D_Graph_Nav/maze_generators/maze_prims_algorithm.cpp
+++ 3D_Graph_Nav/maze_generators/maze_prims_algorithm.cpp
@@ -4,8 +4,47 @@
 #include "maze_prims_algorithm.h"
 
 std::vector<std::pair<int, int>> PrimsMaze::new_frontier_points() {
-   std::vector<std::pair<int,int>> frontier_points;
-   frontier_points.emplace_back(current_pos);
-   return frontier_points;
+    std::vector<std::pair<int,int>> neighbors;
+    int x = current_pos.first, y = current_pos.second;
+
+    //Gather all neighbors
+    //0 == left, 1 == up, 2 == right, 3 == down
+    //ensure that each wall is a valid neighbor.
+    //the wall must not be our outer border and
+    //the position must be a wall not in the frontier already
+    // (set ensures no duplicates) - don't need to check in our code
+    if(x-1 > 0)
+        neighbors.emplace_back(x-1,y);
+    if(y+1 < graphHeight*2 - 2)
+        neighbors.emplace_back(x,y+1);
+    if(x + 1 < graphWidth*2 - 2)
+        neighbors.emplace_back(x+1,y);
+    if(y-1 > 0)
+        neighbors.emplace_back(x,y-1);
+
+    return neighbors;
+}
+
+void PrimsMaze::generate_maze() {
+    //Initialize grid as walls
+    set_grid(1);
+
+    //Add neighboring tiles to the frontier list.
+    std::vector<std::pair<int, int>> l = new_frontier_points();
+    for(auto & i : l){
+        frontier.insert(i);
+    }
+    /* While the frontier list is not empty:
+               Randomly select a wall tile from the frontier list.
+               Check if the wall tile separates a passage from another wall tile:
+                   If true, convert the wall tile into a passage (value 0).
+                   Also, convert the wall tile's neighboring wall (in the direction of the passage) into a passage.
+               Add all neighboring wall tiles of the newly created passage to the frontier list (if they are not already in the list)
+    */
+    while(!frontier.empty()){
+
+    }
+
+
 }
 
--- 3D_Graph_Nav/maze_generators/maze_prims_algorithm.h
+++ 3D_Graph_Nav/maze_generators/maze_prims_algorithm.h
@@ -5,19 +5,24 @@
 #ifndef ADVANCED_GRAPH_TRAVERSAL_VISUALIZATION_MAZE_PRIMS_ALGORITHM_H
 #define ADVANCED_GRAPH_TRAVERSAL_VISUALIZATION_MAZE_PRIMS_ALGORITHM_H
 #include "maze.h"
+#include <unordered_set>
+//For use with the unordered set! this will give us very quick computation because
+//unordered set has O(1) access time. (Hash)
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2>& pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
 class PrimsMaze : public Maze{
 private:
     //This function will save its wall breaking history in the wall_history vector that it inherits
     //Generates a maze using Prim's Algorithm
-    void generate_maze() override {
-
-    }
+    void generate_maze() override;
     std::vector<std::pair<int,int>> new_frontier_points();
-
+    std::unordered_set<std::pair<int, int>, pair_hash> frontier;
 public:
     PrimsMaze(int startX, int startY, int graphWidth, int graphHeight)
-            : Maze(startX, startY, graphWidth, graphHeight) {
-        set_grid(1);
-    }
+            : Maze(startX, startY, graphWidth, graphHeight) {}
 };
 #endif //ADVANCED_GRAPH_TRAVERSAL_VISUALIZATION_MAZE_PRIMS_ALGORITHM_H
